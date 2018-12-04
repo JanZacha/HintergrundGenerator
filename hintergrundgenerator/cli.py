1aff05e989a5fc64c0037eb40467b87063a49eef
Jan Zacharias <jan.zacharias@gmx.net>
2018-12-04 20:58:44 +0100

    Initial commit

--- hintergrundgenerator/cli.py
+++ hintergrundgenerator/cli.py
@@ -2,16 +2,54 @@
 
 """Console script for hintergrundgenerator."""
 import sys
+from random import random, gauss
+
 import click
+import svgwrite
+
+from hintergrundgenerator.hintergrundgenerator import get_coords_random, map_opacity_to_layer
 
 
 @click.command()
-def main(args=None):
+@click.option('--out', default='output.svg', help='Output image name')
+@click.option('--layers', default=10, help='Number of layers.')
+@click.option('--max-size', default=10)
+@click.option('--image-w', default=800, help='Width of the output image in pixels')
+@click.option('--image-h', default=600, help='Height of the output image in pixels')
+@click.option('--bg-color', default='#0563a5', help='Background color')
+def main(out, layers, max_size, image_w, image_h, bg_color):
     """Console script for hintergrundgenerator."""
-    click.echo("Replace this message by putting your code into "
-               "hintergrundgenerator.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
-    return 0
+
+    dwg = svgwrite.Drawing(out, (image_w, image_h), debug=True)
+    dwg.add(dwg.rect(insert=(0, 0), size=('100%', '100%'), fill=bg_color))
+
+    # create simple filters to blur
+    def create_blur_filter(i):
+        blur_filter = dwg.filter()
+        blur_filter.feGaussianBlur(in_='SourceGraphic', stdDeviation=i * .9)
+        return blur_filter
+
+    filters = [create_blur_filter(i) for i in range(layers)]
+
+    for f in filters:
+        dwg.defs.add(f)
+
+    groups = [dwg.add(dwg.g(filter=f.get_funciri())) for f in filters]
+
+    # This is a hack to ensure the groups (aka layers) have the same size as the image
+    # if we do not do this, we get artifacts when the blurred circles extend the group dimensions
+    for g in groups:
+        g.add(dwg.rect(insert=(0, 0), size=('100%', '100%'), fill="#ff0000", fill_opacity=0))
+
+    for x, y in get_coords_random(image_w):
+        opacity = random()
+        size = gauss(max_size, 30)
+        group_index = map_opacity_to_layer(layers, opacity)
+        groups[group_index].add(dwg.circle((x, y), size, fill='white', fill_opacity=opacity))
+
+    dwg.save(pretty=True)
+    # sad. cairo ignores the blurring effect completely!
+    # cairosvg.svg2png(bytestring=dwg.tostring(), write_to='random_circles.png')
 
 
 if __name__ == "__main__":
--- hintergrundgenerator/hintergrundgenerator.py
+++ hintergrundgenerator/hintergrundgenerator.py
@@ -1,3 +1,16 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+from random import random, gauss
+
+
+def map_opacity_to_layer(num_layers, opacity):
+    assert 0 <= opacity <= 1, "Opacity %s not in range 0..1" % opacity
+    return int((1 - opacity) * num_layers)
+
+
+def get_coords_random(width, num_points=200):
+    for i in range(num_points):
+        x = random() * width
+        y = gauss(200, 70)
+        yield x, y
